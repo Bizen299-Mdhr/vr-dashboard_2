64a3172e91a6768fcef1ddc4c811bdc9ec78dd6f
Bijendra <bijendra.manandhar@ekbana.info>
2022-10-08 14:12:14 +0545

    fix: updates

---
+++ src/controllers/frontend/home/homeController.js
@@ -0,0 +1,25 @@
+const Controller = require('@baseController');
+
+class HomeController extends Controller {
+    constructor(opts) {
+        super(opts);
+        this.service = opts.dashboardService;
+        this.title = 'Home';
+        this.view = '../../frontend/home';
+        this.url = '/home';
+        this.module = 'home';
+    }
+  
+    async index(req, res) {
+        try { 
+            this.innerPage = this.view + '/index';
+            const data = {};
+            return res.render('../frontend/layout/base-inner', this.viewData(data, this.module+'view'));
+        } catch (error) {
+            req.flash('error_msg', error.message);
+            return res.redirect('/system/home');
+        }
+    }
+}
+
+module.exports = HomeController;
